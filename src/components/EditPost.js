01fdb61a88e8189e89daea7cb069842b66d7635f
khanr <khanriza@gmail.com>
2020-10-30 22:53:10 -0400

    User can now edit post

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useImmerReducer } from "use-immer";
 import { useParams, Link } from "react-router-dom";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 import Axios from "axios";
+import StateContext from "../StateContext";
+import DispatchContext from "../DispatchContext";
 
 function EditPost() {
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
+
   const originalState = {
     title: {
       value: "",
@@ -23,17 +28,38 @@ function EditPost() {
   };
 
   function ourReducer(draft, action) {
-    switch(action.type) {
-        case "fetchComplete":
-          draft.title.value = action.value.title
-          draft.body.value = action.value.body
-          draft.isFetching = false
-          return
+    switch (action.type) {
+      case "fetchComplete":
+        draft.title.value = action.value.title;
+        draft.body.value = action.value.body;
+        draft.isFetching = false;
+        return;
+      case "titleChange":
+        draft.title.value = action.value;
+        return;
+      case "bodyChange":
+        draft.body.value = action.value;
+        return;
+      case "submitRequest":
+        draft.sendCount++;
+        return;
+      case "saveRequestStarted":
+        draft.isSaving = true;
+        return;
+      case "saveRequestFinished":
+        draft.isSaving = false;
+        return;
     }
   }
 
   const [state, dispatch] = useImmerReducer(ourReducer, originalState);
 
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch({ type: "submitRequest" });
+  }
+
+  // this is to get the post information
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source();
 
@@ -53,11 +79,45 @@ function EditPost() {
     };
   }, []);
 
+  // this is to post changes
+  useEffect(() => {
+    if (state.sendCount) {
+      dispatch({ type: "saveRequestStarted" });
+      const ourRequest = Axios.CancelToken.source();
+
+      async function fetchPost() {
+        try {
+          const response = await Axios.post(
+            `/post/${state.id}/edit`,
+            {
+              title: state.title.value,
+              body: state.body.value,
+              token: appState.user.token,
+            },
+            {
+              cancelToken: ourRequest.token,
+            }
+          );
+          dispatch({ type: "saveRequestFinished" });
+          dispatch({ type: "fetchComplete", value: response.data });
+          appDispatch({ type: "flashMessage", value: "Post was updated" });
+        } catch (e) {
+          console.log("there was a problem");
+        }
+      }
+      fetchPost();
+
+      return () => {
+        ourRequest.cancel();
+      };
+    }
+  }, [state.sendCount]);
+
   if (state.isFetching) return <LoadingDotsIcon />;
 
   return (
     <div className="container pb-2">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="form-group">
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
@@ -70,6 +130,9 @@ function EditPost() {
             type="text"
             placeholder=""
             autoComplete="off"
+            onChange={(e) =>
+              dispatch({ type: "titleChange", value: e.target.value })
+            }
             value={state.title.value}
           />
         </div>
@@ -82,10 +145,15 @@ function EditPost() {
             id="post-body"
             className="body-content tall-textarea form-control"
             type="text"
+            onChange={(e) =>
+              dispatch({ type: "bodyChange", value: e.target.value })
+            }
             value={state.body.value}
           />
         </div>
-        <button className="btn btn-primary">Save Updates</button>
+        <button className="btn btn-primary" disabled={state.isSaving}>
+          Save Updates
+        </button>
       </form>
     </div>
   );
